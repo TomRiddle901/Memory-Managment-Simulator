d89c3ee8f8fe91af00b6e51596037fbeb5742cfb
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-05 11:40:43 +0100

    Implement basic setup and draw functions for 3D canvas rendering

---
+++ script.js
@@ -0,0 +1,14 @@
+function setup(){
+    createCanvas(500, 500, WEBGL); // Dimension of canvas
+}
+
+function draw(){
+    background(220); // Dimension of background
+    background("#c1fff2"); // Color of background
+    rect(100, 100, 100, 123, 123); // rect(x, y, size, height, border-radius)
+    rect(250, 250, 30, 20);
+
+    rotateY(frameCount * 0.01); // Rotation Y of rectangle rotateY(frameCount * speed)
+    rotateX(frameCount * 0.01); // Rotation X of rectangle rotateX(frameCount * speed)
+    rect(0, 0, 150, 150, 123);
+}
