ba636d731ca45eeeb46228a1962e8a4ad0d3ed75
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-12 11:39:43 +0100

    Created mountains

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ function draw(){
     }
 
     // Sun
-    fill("yellow");
+    fill(255, 135, 5, 60);
+    circle(640, sunHeight, 160);
+    fill(255, 100, 0, 100);
     circle(640, sunHeight, 160);
 
     // Line of horizon
@@ -27,4 +29,20 @@ function draw(){
     // Grass
     fill("green");
     rect(0, horizon, 1280, 720);
+
+    // Mountains
+    fill(200, 200, 200);
+    triangle(200, 400, 520, 253, 800, 400);
+    fill(150, 150, 150);
+    triangle(200, 400, 520, 253, 350, 400);
+
+    fill(200, 200, 200);
+    triangle(-40, 400, 150, 200, 400, 400);
+    fill(150, 150, 150);
+    triangle(-40, 400, 150, 200, 50, 400);
+
+    fill(200, 200, 200);
+    triangle(650, 400, 1150, 60, 1350, 400);
+    fill(150, 150, 150);
+    triangle(1600, 400, 1150, 60, 1270, 400);
 }
