4512933b05baad52bdd1a09d9751c32fe1254355
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-12 11:02:33 +0100

    Tutoria p5.js 1: create a sunrise

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function setup(){
-    createCanvas(500, 500, WEBGL); // Dimension of canvas
-}
-
-function draw(){
-    background(220); // Dimension of background
-    background("#c1fff2"); // Color of background
-    rect(100, 100, 100, 123, 123); // rect(x, y, size, height, border-radius)
-    rect(250, 250, 30, 20);
-
-    rotateY(frameCount * 0.01); // Rotation Y of rectangle rotateY(frameCount * speed)
-    rotateX(frameCount * 0.01); // Rotation X of rectangle rotateX(frameCount * speed)
-    rect(0, 0, 150, 150, 123);
-
-    rect(30, 20, 55, 50, 20, 15, 10, 5); // rect(x, y, size, height, border-radius top-left, top-right, bottom-right, bottom-left)
-    rect(30, 20, 60, 60, 20, 15, 10, 5);
-}
+// Sunrise
