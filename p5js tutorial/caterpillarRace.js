88e52cfc33c385ab238221825273562b0e6981e0
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-16 16:37:47 +0100

    Created finish line and start line

--- p5js tutorial/caterpillarRace.js
+++ p5js tutorial/caterpillarRace.js
@@ -1,8 +1,19 @@
 // Coordinates
-let startLine = 20;
-let finishLine = 400;
+let startLine = 40;
+let finishLine = 450;
 
 
 function setup(){
     createCanvas(500, 500);
+}
+
+function draw(){
+    background(121, 96, 76);
+
+    fill(0, 255, 0);
+    rect(finishLine, 0, 20, 500);
+
+    noStroke();
+    fill(0, 0, 0);
+    rect(startLine, 0, 5, 500);
 }
