d5db2128268834884f7cd6f878b99c922e80a352
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-16 16:44:44 +0100

    Implemented caterpillar movement and stop simulation when caterpillar reaches finish line

--- p5js tutorial/caterpillarRace.js
+++ p5js tutorial/caterpillarRace.js
@@ -8,18 +8,30 @@ let circY = 250;
 
 function setup(){
     createCanvas(500, 500);
-    noStroke();
 }
 
 function draw(){
     background(121, 96, 76);
 
+    // Start and finish lines
+    noStroke();
     fill(0, 255, 0);
     rect(finishLine, 0, 20, 500);
 
+    noStroke();
     fill(0, 0, 0);
     rect(startLine, 0, 5, 500);
 
+    // Caterpillar
+    stroke(0);
     fill(255, 0, 255);
     circle(circX, circY, 50);
+
+    // Caterpillar movement
+    circX += 20;
+
+    // Srtop simulation when caterpillar reaches finish line
+    if (circX > finishLine){
+        noLoop();
+    }
 }
