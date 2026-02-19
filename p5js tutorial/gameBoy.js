90ac7d4de4a025d005077483422e02f509992208
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-19 10:08:03 +0100

    Corrected some errors, refractored some comments and variables

--- p5js tutorial/gameBoy.js
+++ p5js tutorial/gameBoy.js
@@ -1,204 +1,209 @@
-let winWidth = 400;
-let winHeight = 300;
-
-function setup(){
+function setup() {
     let canvas = createCanvas(winWidth, winHeight);
 
+    // Create the game boy emulator container
     let gameBoyEmulator = createDiv();
-    gameBoyEmulator.id("game-boy-emulator"); // Game boy emulator container
+    gameBoyEmulator.id('game-boy-emulator');
 
+    // Create the game container
     let gameContainer = createDiv();
-    gameContainer.id("game-container"); // Game boy emulator container
+    gameContainer.id('game-container');
 
-    let scoreContainer = createDiv("Punteggio: ");
-    scoreContainer.id("score-container"); // Game boy emulator score container
-
-    let scoreSpan = createP("0");
-    scoreSpan.id("score"); // Score element
-
-    let gameBoyText = createDiv("GameBoy");
-    gameBoyText.id("game-boy-text"); // Game boy text
-
-    let buttonContainer = createDiv();
-    buttonContainer.id("button-container"); // Game boy buttons container
-
-    let arrowButtons = createDiv();
-    arrowButtons.id("arrow-buttons"); // Game boy arrow buttons container
-
-    let leftRightButtons = createDiv();
-    leftRightButtons.id("leftRightButton"); // Game boy left/right buttons
-
-    let actionButtons = createDiv();
-    actionButtons.id("action-buttons"); // Game boy action buttons
-
-    let leftRightContainer = createDiv();
-    leftRightContainer.id("leftRightContainer"); // Game Boy left right buttons container
-
-    let body = select('body');
-
-    // Connecting containers to the body
-    body.child(gameBoyEmulator);
-    gameBoyEmulator.child(gameContainer);
-    gameBoyEmulator.child(gameBoyText);
-    gameBoyEmulator.child(buttonContainer);
-
-    // Connecting elements to the respective containers
-    scoreContainer.child(scoreSpan);
-    gameContainer.child(scoreContainer);
-    gameContainer.child(canvas);
-    buttonContainer.child(arrowButtons);
-    buttonContainer.child(actionButtons);
-
-    // Style for the Game Boy emulator container
-    gameBoyEmulator.style("background-color", "#8b8b8b");
-    gameBoyEmulator.style("border", "10px solid #000");
-    gameBoyEmulator.style("border-radius", "10px");
-    gameBoyEmulator.style("padding", "20px");
-    gameBoyEmulator.style("box-shadow", "0 0 20px rgba(0, 0, 0, 0.8)");
-
-    // Style for the button container
-    buttonContainer.style("display", "flex");
-    buttonContainer.style("align-items", "center");
-    buttonContainer.style("justify-content", "space-between");
-    buttonContainer.style("margin-top", "20px");
-
-    // Style for the arrow button container
-    arrowButtons.style("display", "flex");
-    arrowButtons.style("flex-direction", "column");
-    arrowButtons.style("align-items", "center");
-
-    // Style for the action buttons container
-    actionButtons.style("display", "flex");
-    actionButtons.style("align-items", "center");
-
-    // Style for Game Boy score container
-    scoreContainer.style("position", "absolute");
-    scoreContainer.style("margin-left", "340px");
-    scoreContainer.style("color", "#fff");
-    scoreContainer.style("margin-top", "5px");
-
-    // Style for Game Boy text
-    gameBoyText.style("margin", "10px 145px");
-    gameBoyText.style("font-size", "25px");
-    gameBoyText.style("color", "#fff");
+    // Create the game boy text
+    let gameBoyText = createDiv('GameBoy');
+    gameBoyText.id('game-boy-text');
+    gameBoyText.style('margin', '10px 145px');
+    gameBoyText.style('font-size', '25px');
+    gameBoyText.style('color', '#fff');
     gameBoyText.style("background-color", "#0077b6");
-    gameBoyText.style("padding", "5px");
-    gameBoyText.style("border-radius", "5px");
+    gameBoyText.style('padding', '10px');
+    gameBoyText.style('border-radius', '5px');
 
-    // Buttons
-    let upButton = createButton('▲');
+    gameBoyText.style('font-family', 'PressStart2P-Regular');
+
+    // Create the button container
+    let buttonContainer = createDiv();
+    buttonContainer.id('button-container');
+
+    // Create the arrow buttons container
+    let arrowButtons = createDiv();
+    arrowButtons.id('arrow-buttons');
+
+    // Create an up button
+    let upButton = createButton('&#9650;');
     upButton.id('up');
-    // Up button style
+    // Set its styles
     upButton.style('color', 'white');
     upButton.style('background-color', 'red');
     upButton.style('width', '40px');
     upButton.style('height', '40px');
+    arrowButtons.child(upButton);
     upButton.style('margin-bottom', '10px');
     upButton.style('border-radius', '5px');
-    // Connection to the buttons container
-    arrowButtons.child(upButton);
+    // Add a callback function
+    upButton.mouseClicked(goUp);
 
-    // Connect arrow buttons to the buttons container
+    // Create the container for the left and right buttons
+    let leftRightContainer = createDiv();
     arrowButtons.child(leftRightContainer);
 
-    let leftButton = createButton('◀');
+    // Create the left button
+    let leftButton = createButton('&#9664;');
     leftButton.id('left');
-    // Left button style
+    // Set its styles
     leftButton.style('color', 'white');
     leftButton.style('background-color', 'red');
     leftButton.style('width', '40px');
     leftButton.style('height', '40px');
     leftButton.style('margin-right', '30px');
     leftButton.style('border-radius', '5px');
-    // Connection to the buttons container
+    // Add it to the leftRightContainer
     leftRightContainer.child(leftButton);
+    // Add a callback function
+    leftButton.mouseClicked(goLeft);
 
-    let rightButton = createButton('▶');
+    // Create the right button
+    let rightButton = createButton('&#9654;');
     rightButton.id('right');
-    // Right button style
+    // Set its styles
     rightButton.style('color', 'white');
     rightButton.style('background-color', 'red');
     rightButton.style('width', '40px');
     rightButton.style('height', '40px');
     rightButton.style('margin-left', '5px');
     rightButton.style('border-radius', '5px');
-    // Connection to the buttons container
+    // Add it to the leftRightContainer
     leftRightContainer.child(rightButton);
+    // Add a callback function
+    rightButton.mouseClicked(goRight);
 
-    let downButton = createButton('▼');
-    // Connection to the buttons container
+    // Create the down button
+    let downButton = createButton('&#9660;');
     arrowButtons.child(downButton);
     downButton.id('down');
-    // Down button style
+    // Set its styles
     downButton.style('color', 'white');
     downButton.style('background-color', 'red');
     downButton.style('width', '40px');
     downButton.style('height', '40px');
     downButton.style('margin-top', '10px');
     downButton.style('border-radius', '5px');
+    // Add a callback function
+    downButton.mouseClicked(goDown);
 
+    // Create the action buttons container
+    let actionButtons = createDiv();
+    actionButtons.id('action-buttons');
+
+    // Create the play button
     let playButton = createButton('▶');
-    // Connection to the action buttons container
     actionButtons.child(playButton);
     playButton.id('play');
-    // Play button style
-    playButton.style('background-color', '#0000ff');
+    // Set its styles
+    playButton.style('background-color', 'blue');
     playButton.style('color', 'white');
     playButton.style('width', '60px');
     playButton.style('height', '60px');
-    playButton.style('border-radius','50%');
+    playButton.style('font-size', '24px');
+    // Make it a circle
+    playButton.style('border-radius', '50%');
+    // Add spacing
     playButton.style('margin-right', '10px');
+    // Add a callback function
+    playButton.mouseClicked(startGame);
 
+    // Create the pause button
     let pauseButton = createButton('❚❚');
     pauseButton.id('pause');
-    // Pause button style
-    pauseButton.style('background-color', '#0000ff');
+    // Set its styles
+    pauseButton.style('background-color', 'blue');
     pauseButton.style('color', 'white');
     pauseButton.style('width', '60px');
     pauseButton.style('height', '60px');
     pauseButton.style('font-size', '24px');
+    // Make it a circle
     pauseButton.style('border-radius', '50%');
-    // connection to the action buttons container
     actionButtons.child(pauseButton);
+    // Add a callback function
+    pauseButton.mouseClicked(pauseGame);
+
+    // Set styles for the game boy emulator container
+    gameBoyEmulator.style('background-color', '#8b8b8b');
+    gameBoyEmulator.style('border', '10px solid #000');
+    gameBoyEmulator.style('border-radius', '10px');
+    gameBoyEmulator.style('padding', '20px');
+
+    // Set styles for the button container
+    buttonContainer.style('display', 'flex');
+    buttonContainer.style('align-items', 'center');
+    buttonContainer.style('justify-content', 'space-between');
+    buttonContainer.style('margin-top', '20px');
+
+    // Set styles for the arrow buttons container
+    arrowButtons.style('display', 'flex');
+    arrowButtons.style('flex-direction', 'column');
+    arrowButtons.style('align-items', 'center');
+
+    // Set styles for the action buttons container
+    actionButtons.style('display', 'flex');
+    actionButtons.style('align-items', 'center');
+
+    // Add elements to their respective containers
+    gameContainer.child(canvas);
+    buttonContainer.child(arrowButtons);
+    buttonContainer.child(actionButtons);
+
+    // Add containers to the main container
+    gameBoyEmulator.child(gameContainer);
+    gameBoyEmulator.child(gameBoyText);
+    gameBoyEmulator.child(buttonContainer);
+
+    // Add the gameBoyEmulator to the sketch
+    let body = select('body');
+    // Add emulator as the first child element in the page's body.
+    body.child(gameBoyEmulator);
 
     // snakeGame.js
-    frameRate(fps); // set the framerate of the screen
-    moveFood(); // move the food to a random location
-    resetSnake(); // reset the snake
-    playButton.mousePressed(startGame); // start game on button click
-    pauseButton.mousePressed(pasueGame); // pause game on button click
+    frameRate(fps);
+
+    // Move the food to a random location.
+    moveFood();
+
+    // Reset the snake.
+    resetSnake();
 }
 
-function draw(){
+function draw() {
     background(51);
 
-    // If the game is not started
-    if (gameStarted === false){
+    // Skip the rest of the draw function if the game hasn't started.
+    if (gameStarted === false) {
         displayStartMessage();
         return;
     }
 
-    // Update the snake state
-    if (gamePaused === false){
+    // Update the game state.
+    if (gamePaused === false) {
         moveSnake();
         checkEdges();
-        checkFood();
         checkSelf();
+        checkFood();
         updateBody();
     }
 
-    // Draw the snake and the food
+    // Draw the snake and its food.
     drawFood();
     drawSnake();
 
-    // If game has ended
-    if (gameOver === true){
+    // Skip the rest of the draw function if the game has ended
+    if (gameOver === true) {
         displayEndMessage();
         noLoop();
     }
 
-    // Update the score
+    // Update the score.
     textAlign(RIGHT);
     fill(255);
-    text(`Punteggio: ${score}`, width - 10, 20);
+    text(`Score: ${score}`, width - 10, 20);
+    textSize(15)
 }
