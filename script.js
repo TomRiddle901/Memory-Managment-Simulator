2b53ef36a5a23d5aa6d5101675eb2004298434b2
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-19 11:42:14 +0100

    Implemented principal rect for RAM and principal rect for SO

--- script.js
+++ script.js
@@ -9,4 +9,29 @@ function setup(){
 
 function draw(){
     background(200, 200, 200);
+
+    // Div rettangolo RAM
+    let ramDiv = createDiv();
+    ramDiv.id("ramDiv");
+
+    ramDiv.child(
+        fill(255, 255, 255),
+        rect(67, 40, 300, 600));
+
+    // DIV sistema operativo (rimane sempre alla stessa dimensione)
+    let systemProcessDiv = createDiv();
+    systemProcessDiv.id('systemProcessDiv');
+
+    systemProcessDiv.child(
+        fill(150, 140, 40),
+        rect(67, 40, 300, 30));
+    systemProcessDiv.child(
+        fill(0),
+        textSize(15),
+        text("Sistema Operativo", 150, 60));
+
+    // Aggiungi tutto al body
+    let body = select('body');
+    body.child(ramDiv);
+    body.child(systemProcessDiv);
 }
